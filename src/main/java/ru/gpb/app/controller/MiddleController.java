bfab207246ee10c0ab1ba836c0a0ff3f418a3b28
Timur <timk01@mail.ru>
2024-06-11 17:41:31 +0300

    Добавлен /v2 в соответствии с АПИ.

--- src/main/java/ru/gpb/app/controller/MiddleController.java
+++ src/main/java/ru/gpb/app/controller/MiddleController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v2/api")
 public class MiddleController {
 
     private final UserMiddleService userMiddleService;
